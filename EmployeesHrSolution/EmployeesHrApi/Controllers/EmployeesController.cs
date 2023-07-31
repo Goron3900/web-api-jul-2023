a3ffbf27bf17fcaaf2ab7ee2948b3ca520f76d23
Zachary Janouskovec <zjanouskovec@gmail.com>
2023-07-31 16:36:28 -0400

    Finished Monday

--- EmployeesHrSolution/EmployeesHrApi/Controllers/EmployeesController.cs
+++ EmployeesHrSolution/EmployeesHrApi/Controllers/EmployeesController.cs
@@ -1,13 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeesHrApi.Data;
+using EmployeesHrApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeesHrApi.Controllers;
 
 public class EmployeesController : ControllerBase
 {
+    private readonly EmployeeDataContext _context;
+
+    private readonly Employee employee = new Employee()
+    { 
+        FirstName = "Zachary",
+        LastName = "Janouskovec"
+    };
+
+    public EmployeesController(EmployeeDataContext context)
+    {
+        _context = context;
+    }
+
     // GET /employees
     [HttpGet("/employees")]
     public async Task<ActionResult> GetEmployeesAsync() 
     {
-        return Ok();
+        var employees = await _context.Employees
+            .Select(emp => new EmployeesSummaryResponseModel
+            {
+                Id = emp.Id.ToString(),
+                FirstName = emp.FirstName,
+                LastName = emp.LastName,
+                Department = emp.Department,
+                Email = emp.Email,
+
+            })
+            .ToListAsync();
+
+        var response = new EmployeesResponseModel
+        {
+            Employees = employees
+        };
+
+        return Ok(response);
     }
 }
--- EmployeesHrSolution/EmployeesHrApi/Program.cs
+++ EmployeesHrSolution/EmployeesHrApi/Program.cs
@@ -1,4 +1,7 @@
 
+using EmployeesHrApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace EmployeesHrApi
 {
     public class Program
@@ -6,6 +9,7 @@ namespace EmployeesHrApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var employeesConnectionString = builder.Configuration.GetConnectionString("employees") ?? throw new Exception("Need a Connection String");
 
             // Add services to the container.
 
@@ -13,6 +17,11 @@ namespace EmployeesHrApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<EmployeeDataContext>(options =>
+            {
+                options.UseSqlServer(employeesConnectionString);
+            });
 
             var app = builder.Build();
 
